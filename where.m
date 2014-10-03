1250b87e1abf96f0c79f64fff9d9e5290f8fd31f
lionel-rigoux <lionel.rigoux@gmail.com>
2014-10-03 10:45:29 +0200

    better handling of multiple fields in where

--- where.m
+++ where.m
@@ -2,33 +2,33 @@ function [subStructList,idx] = where(StructList,fieldName,value)
 % WHERE filter a list of structure such that StructList.(...).fieldName == value
 
 filedNameList = fieldnames(StructList);
+found = false;
 
-found = 0;
 if isfield(StructList(1),fieldName)
     %___________________________________________________________
     % try first level
     idx = find(cellfun(@(x) isequal(x,value), {StructList.(fieldName)}));
-    found = 1;
+    found = true;
 else
     %___________________________________________________________
     % try nested structures
     for iField = 1:numel(filedNameList)
         if isstruct(StructList(1).(filedNameList{iField}))
-            [~,idx] = where([StructList.(filedNameList{iField})],fieldName,value);
-            found = found + 1;
+            [~,idxTemp] = where([StructList.(filedNameList{iField})],fieldName,value);
+            if numel(idxTemp)<numel(StructList)
+                if found && ~isempty(setdiff(idxTemp,idx))
+                        	error('Multiple incongruent fields named ''%s'' found',fieldName);
+                end
+                found = true;
+                idx = idxTemp;
+            end
         end
     end
 end
 
-switch found
-    case 0
-        warning('No field ''%s''  found in the structure',fieldName);
-        idx = 1:numel(StructList);
-    case 1
-    otherwise
-        error('Multiple fields named ''%s'' found',fieldName);
+if ~found
+    idx = 1:numel(StructList);
 end
-
 subStructList = StructList(idx);
 
 
