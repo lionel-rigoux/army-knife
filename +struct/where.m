4ec1543e8fabdb9b6e79d0b3f5c9c31d78291e06
Lionel RIGOUX <lionel.rigoux@gmail.com>
2014-10-06 18:26:16 +0200

    struct.where refactor (now uses extract)

--- +struct/where.m
+++ +struct/where.m
@@ -1,37 +1,16 @@
-function [subStructList,idx] = where(StructList,fieldName,value)
+function [subStructList,idx] = where(structList,fieldName,value)
 % WHERE filter a list of structure such that StructList.(...).fieldName == value
+% 
 
-filedNameList = fieldnames(StructList);
-found = false;
+ valueList = struct.extract(structList,fieldName);
+ if isnumeric(valueList)
+     valueList = num2cell(valueList);
+ end
+ 
+idx = cellfun(@(x) isequal(x,value), valueList );
+subStructList = structList(idx);
 
-if isfield(StructList(1),fieldName)
-    %___________________________________________________________
-    % try first level
-    idx = find(cellfun(@(x) isequal(x,value), {StructList.(fieldName)}));
-    found = true;
-else
-    %___________________________________________________________
-    % try nested structures
-    for iField = 1:numel(filedNameList)
-        if isstruct(StructList(1).(filedNameList{iField}))
-            [~,idxTemp] = where([StructList.(filedNameList{iField})],fieldName,value);
-            if numel(idxTemp)<numel(StructList)
-                if found && ~isempty(setdiff(idxTemp,idx))
-                        	error('Multiple incongruent fields named ''%s'' found',fieldName);
-                end
-                found = true;
-                idx = idxTemp;
-            end
-        end
-    end
-end
-
-if ~found
-    idx = 1:numel(StructList);
-end
-subStructList = StructList(idx);
-
-
+idx = find(idx);
 
 
 
