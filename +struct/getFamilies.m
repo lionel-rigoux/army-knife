4e8dd5b5c0a5470a9b80e95c6ff746fbcbffb262
Lionel RIGOUX <lionel.rigoux@gmail.com>
2014-10-06 18:26:47 +0200

    struct.getFamilies work for multiple fields caters

--- +struct/getFamilies.m
+++ +struct/getFamilies.m
@@ -1,26 +1,75 @@
-function familyStruct = getFamilies(structList,fieldName)
+function familyStruct = getFamilies(structList,fieldNameList,prefix,idx)
 
-    valueList = struct.extract(structList,fieldName);
+    if ~exist('prefix'), prefix = ''                 ; end
+    if ~exist('idx')   , idx    = 1:numel(structList); end
+
+    if iscell(fieldNameList) && numel(fieldNameList) == 1
+        fieldNameList = fieldNameList{1};
+    end
+%______________________________________________________________________
+% simple family
+if isstr(fieldNameList) % not a list
+
     
+    fieldName = fieldNameList;
     
-    familiesNames = sort(unique(valueList));
+    % find list of levels
+    valueList = struct.extract(structList,fieldName) ;
+    familiesName = unique(valueList,'sorted');
     
+    % iterate over levels to find indices
     families = {};
-    for iF = 1:numel(familiesNames)
+    for iF = 1:numel(familiesName)
         
         if iscell(valueList)
-            familyValue = familiesNames{iF};
+            familyValue = familiesName{iF};
             isInFamily = cellfun(@(v) isequal(v,familyValue), valueList);
         else
-            familyValue = familiesNames(iF);
+            familyValue = familiesName(iF);
             isInFamily = (valueList == familyValue);
         end
-        families{iF} = find(isInFamily);
+        families{iF} = idx(isInFamily);
     end
     
-    if isnumeric(familiesNames)
-        familiesNames = cellfun(@(x) {num2str(x)},num2cell(familiesNames));
-    end
-    familyStruct.familiesName = familiesNames ;
+    % storage
     familyStruct.families = families;
+    
+    % ensure string storage of family level
+    
+    if isnumeric(familiesName)
+        familiesNameLabel = cellfun(@(x) {num2str(x)}, num2cell(familiesName)) ;
+    else
+        familiesNameLabel = familiesName ;
+    end
+    familyStruct.familiesName = cellfun(@(x) {[prefix fieldName ':' x]}, familiesNameLabel) ; 
         
+ 
+%______________________________________________________________________
+% conjunction family
+else
+    familyStruct = struct('families',{{}},'familiesName',{{}});
+    
+    familyStruct_n = struct.getFamilies(structList,fieldNameList{1});
+    
+    for iF=1:numel(familyStruct_n.families)
+        prefix = [familyStruct_n.familiesName{iF} ' & '];
+        idx = familyStruct_n.families{iF};
+        familyStruct_np1 = struct.getFamilies(structList(idx),fieldNameList(2:end),prefix,idx); 
+        
+        familyStruct.families = {familyStruct.families{:} familyStruct_np1.families{:}};
+        familyStruct.familiesName = {familyStruct.familiesName{:} familyStruct_np1.familiesName{:}};
+        
+    end
+    
+end
+
+
+
+
+end
+
+
+function familyStruct = getFamilies_prefixed(structList,fieldNameList)
+
+    
+end
